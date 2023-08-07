2cc75a732ad3d38c58c6c7c51e50d9ddfd3d6d72
LaupWing <loc_strict@hotmail.com>
2023-08-07 16:54:26 +0200

    can appply colors

--- app/routes/($locale)._index.tsx
+++ app/routes/($locale)._index.tsx
@@ -1,95 +1,95 @@
-import type {V2_MetaFunction} from '@shopify/remix-oxygen';
-import {defer, type LoaderArgs} from '@shopify/remix-oxygen';
-import {Await, useLoaderData, Link} from '@remix-run/react';
-import {Suspense} from 'react';
-import {Image, Money} from '@shopify/hydrogen';
+import type { V2_MetaFunction } from '@shopify/remix-oxygen';
+import { defer, type LoaderArgs } from '@shopify/remix-oxygen';
+import { Await, useLoaderData, Link } from '@remix-run/react';
+import { Suspense } from 'react';
+import { Image, Money } from '@shopify/hydrogen';
 import type {
-  FeaturedCollectionFragment,
-  RecommendedProductsQuery,
+   FeaturedCollectionFragment,
+   RecommendedProductsQuery,
 } from 'storefrontapi.generated';
 
 export const meta: V2_MetaFunction = () => {
-  return [{title: 'Hydrogen | Home'}];
+   return [{ title: 'Hydrogen | Home' }];
 };
 
-export async function loader({context}: LoaderArgs) {
-  const {storefront} = context;
-  const {collections} = await storefront.query(FEATURED_COLLECTION_QUERY);
-  const featuredCollection = collections.nodes[0];
-  const recommendedProducts = storefront.query(RECOMMENDED_PRODUCTS_QUERY);
+export async function loader({ context }: LoaderArgs) {
+   const { storefront } = context;
+   const { collections } = await storefront.query(FEATURED_COLLECTION_QUERY);
+   const featuredCollection = collections.nodes[0];
+   const recommendedProducts = storefront.query(RECOMMENDED_PRODUCTS_QUERY);
 
-  return defer({featuredCollection, recommendedProducts});
+   return defer({ featuredCollection, recommendedProducts });
 }
 
 export default function Homepage() {
-  const data = useLoaderData<typeof loader>();
-  return (
-    <div className="home">
-      <div className="container mx-auto">
-        <FeaturedCollection collection={data.featuredCollection} />
-        <RecommendedProducts products={data.recommendedProducts} />
+   const data = useLoaderData<typeof loader>();
+   return (
+      <div className="home">
+         <div className="container mx-auto">
+            <FeaturedCollection collection={data.featuredCollection} />
+            <RecommendedProducts products={data.recommendedProducts} />
+         </div>
       </div>
-    </div>
-  );
+   );
 }
 
 function FeaturedCollection({
-  collection,
+   collection,
 }: {
-  collection: FeaturedCollectionFragment;
+   collection: FeaturedCollectionFragment;
 }) {
-  const image = collection.image;
-  return (
-    <Link
-      className="featured-collection"
-      to={`/collections/${collection.handle}`}
-    >
-      {image && (
-        <div className="featured-collection-image">
-          <Image data={image} sizes="100vw" />
-        </div>
-      )}
-      <h1>{collection.title}</h1>
-    </Link>
-  );
+   const image = collection.image;
+   return (
+      <Link
+         className="featured-collection"
+         to={`/collections/${collection.handle}`}
+      >
+         {image && (
+            <div className="featured-collection-image">
+               <Image data={image} sizes="100vw" />
+            </div>
+         )}
+         <h1>{collection.title}</h1>
+      </Link>
+   );
 }
 
 function RecommendedProducts({
-  products,
+   products,
 }: {
-  products: Promise<RecommendedProductsQuery>;
+   products: Promise<RecommendedProductsQuery>;
 }) {
-  return (
-    <div className="recommended-products">
-      <h2>Recommended Products</h2>
-      <Suspense fallback={<div>Loading...</div>}>
-        <Await resolve={products}>
-          {({products}) => (
-            <div className="recommended-products-grid">
-              {products.nodes.map((product) => (
-                <Link
-                  key={product.id}
-                  className="recommended-product"
-                  to={`/products/${product.handle}`}
-                >
-                  <Image
-                    data={product.images.nodes[0]}
-                    aspectRatio="1/1"
-                    sizes="(min-width: 45em) 20vw, 50vw"
-                  />
-                  <h4>{product.title}</h4>
-                  <small>
-                    <Money data={product.priceRange.minVariantPrice} />
-                  </small>
-                </Link>
-              ))}
-            </div>
-          )}
-        </Await>
-      </Suspense>
-      <br />
-    </div>
-  );
+   return (
+      <div className="recommended-products">
+         <h2>Recommended Products</h2>
+         <Suspense fallback={<div>Loading...</div>}>
+            <Await resolve={products}>
+               {({ products }) => (
+                  <div className="recommended-products-grid">
+                     {products.nodes.map((product) => (
+                        <Link
+                           key={product.id}
+                           className="recommended-product"
+                           to={`/products/${product.handle}`}
+                        >
+                           <Image
+                              data={product.images.nodes[0]}
+                              aspectRatio="1/1"
+                              sizes="(min-width: 45em) 20vw, 50vw"
+                           />
+                           <h4>{product.title}</h4>
+                           <small>
+                              <Money data={product.priceRange.minVariantPrice} />
+                           </small>
+                        </Link>
+                     ))}
+                  </div>
+               )}
+            </Await>
+         </Suspense>
+         <br />
+      </div>
+   );
 }
 
 const FEATURED_COLLECTION_QUERY = `#graphql
